f22bdfccf09766f3677121f687d39959e464c15c
Alexander Gorban <gorban.alexander.y@gmail.com>
2019-07-15 19:53:38 +0300

    Added GetStocks() method for Search Click button

--- StockAnalyzer.Windows/MainWindow.xaml.cs
+++ StockAnalyzer.Windows/MainWindow.xaml.cs
@@ -27,26 +27,33 @@ namespace StockAnalyzer.Windows
             StockProgress.IsIndeterminate = true;
             #endregion
 
-            try
-            {
-                using (var client = new HttpClient())
-                {
-                    var result = await client.GetAsync($"http://localhost:61363/api/stocks/{Ticker.Text}");
-                    var content = await result.Content.ReadAsStringAsync();
-                    var data = JsonConvert.DeserializeObject<IEnumerable<StockPrice>>(content);
-
-                    Stocks.ItemsSource = data;
-                }
-            }
-            catch (Exception ex)
-            {
-                Notes.Text += ex.Message;
-            }
+            await GetStocks();
 
             #region After stock data is loaded
             StocksStatus.Text = $"Loaded stocks for {Ticker.Text} in {watch.ElapsedMilliseconds}ms";
             StockProgress.Visibility = Visibility.Hidden;
             #endregion
+        }
+
+        public async Task GetStocks()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync($"http://localhost:61363/api/stocks/{Ticker.Text}");
+
+                try
+                {
+                    response.EnsureSuccessStatusCode();
+                    var content = await response.Content.ReadAsStringAsync();
+                    var data = JsonConvert.DeserializeObject<IEnumerable<StockPrice>>(content);
+
+                    Stocks.ItemsSource = data;
+                }
+                catch (Exception ex)
+                {
+                    Notes.Text += ex.Message;
+                }
+            }
         }
                 
         private void Hyperlink_OnRequestNavigate(object sender, RequestNavigateEventArgs e)
